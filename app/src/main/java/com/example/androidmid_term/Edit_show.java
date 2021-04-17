d6239924484a56363b93e9b45eda580a6dc01740
TTVI <=>
2021-04-17 13:27:28 +0700

    Vi nop bai

---
+++ app/src/main/java/com/example/androidmid_term/Edit_show.java
@@ -0,0 +1,14 @@
+package com.example.androidmid_term;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_show);
+    }
+}
