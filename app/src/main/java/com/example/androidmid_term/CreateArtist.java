854359a10722036a00036d5a7f9641ff72070edf
dmtri130699 <dmtri130699@gmail.com>
2021-04-17 13:39:24 +0700

    Update Activity

--- app/src/main/java/com/example/androidmid_term/CreateArtist.java
+++ app/src/main/java/com/example/androidmid_term/CreateArtist.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateArtist extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_artist);
     }
 }
