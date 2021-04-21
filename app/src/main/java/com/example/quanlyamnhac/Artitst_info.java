c5e6ee51df16003aa34f538b8b1607ceea0646e2
DESKTOP-K3G3SVR\Vũ Hoàng <vuhoang24c@gmail.com>
2021-04-21 22:30:20 +0700

    Home-Artist & Artist info- Song List Class Artist + mapping

---
+++ app/src/main/java/com/example/quanlyamnhac/Artitst_info.java
@@ -0,0 +1,14 @@
+package com.example.quanlyamnhac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Artitst_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artitst_info);
+    }
+}
