d5b4478c1cd1b810a89511aa3d572e3abeb9dd03
dmtri130699 <dmtri130699@gmail.com>
2021-04-23 17:17:41 +0700

    Create Artist

--- app/src/main/java/com/example/androidmid_term/Database.java
+++ app/src/main/java/com/example/androidmid_term/Database.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 
 import androidx.annotation.Nullable;
 
@@ -24,6 +25,42 @@ public class Database extends SQLiteOpenHelper {
         return database.rawQuery(sql,null);
     }
 
+    public void Insert_Artist_Img(Artist art){
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "INSERT INTO CASI VALUES(null,?,?)";
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+
+        statement.bindString(1,art.getName());
+        statement.bindBlob(2,art.getImg());
+
+        statement.executeInsert();
+    }
+
+    public int get_last_id(){
+        SQLiteDatabase database = getReadableDatabase();
+        String sql ="SELECT MACS FROM CASI ORDER BY MACS DESC LIMIT 1;";
+        Cursor c = database.rawQuery(sql,null);
+
+        int id=0;
+        while (c.moveToNext()){
+            id = c.getInt(0);
+        }
+        return id;
+    }
+
+    public void insert_song_artist(Song song){
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "INSERT INTO BAIHAT VALUES(null,?,?,?)";
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+
+        statement.bindString(1,song.getName());
+        statement.bindString(2,song.getYear());
+        statement.bindString(3,""+song.getId_artist());
+
+        statement.executeInsert();
+    }
     @Override
     public void onCreate(SQLiteDatabase db) {
 
---
+++ app/src/main/java/com/example/androidmid_term/Song.java
@@ -0,0 +1,45 @@
+package com.example.androidmid_term;
+
+public class Song {
+    private int id;
+    private String name;
+    private String year;
+    private int id_artist;
+
+    public Song(String name, String year, int id_artist) {
+        this.name = name;
+        this.year = year;
+        this.id_artist = id_artist;
+    }
+
+    public Song() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getId_artist() {
+        return id_artist;
+    }
+
+    public void setId_artist(int id_artist) {
+        this.id_artist = id_artist;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
