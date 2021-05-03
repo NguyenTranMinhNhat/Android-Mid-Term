478d1d65fbdb72745fc69f061959a26c488e8713
dmtri130699 <dmtri130699@gmail.com>
2021-05-03 21:14:43 +0700

    Add database MidTerm.db

--- app/src/main/java/com/example/androidmid_term/ArrayAdapter_Artist_list.java
+++ app/src/main/java/com/example/androidmid_term/ArrayAdapter_Artist_list.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +20,10 @@ public class ArrayAdapter_Artist_list extends ArrayAdapter<Artist> {
     Context context;
     int resource;
     ArrayList<Artist> data;
+
+    //Filter
+    CustomFilter filter;
+    ArrayList<Artist> filter_list;
 
     @NonNull
     @Override
@@ -36,6 +41,19 @@ public class ArrayAdapter_Artist_list extends ArrayAdapter<Artist> {
         this.context = context;
         this.resource = resource;
         this.data = data;
+
+        //Filter
+        this.filter_list = data;
+    }
+
+    //Filter
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        if(filter == null ){
+            filter = new CustomFilter();
+        }
+        return filter;
     }
 
     @NonNull
@@ -59,4 +77,48 @@ public class ArrayAdapter_Artist_list extends ArrayAdapter<Artist> {
         Bitmap bitmap = BitmapFactory.decodeByteArray(img, 0, img.length);
         return bitmap;
     }
+    //Inner class
+    class CustomFilter extends Filter{
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            if(constraint != null && constraint.length() > 0){
+                // Constrain to upper
+                constraint = constraint.toString().toUpperCase();
+                ArrayList<Artist> filters = new ArrayList<>();
+
+                // get specific item
+                for(int i=0; i < filter_list.size(); i++){
+                    if(filter_list.get(i).getName().contains(constraint)){
+                        Artist a = new Artist();
+                        a.setImg(filter_list.get(i).getImg());
+                        a.setName(filter_list.get(i).getName());
+                        filters.add(a);
+                    }
+                }
+                results.count = filters.size();
+                results.values = filters;
+            }
+            else{
+                results.count = filter_list.size();
+                results.values = filter_list;
+
+            }
+
+            return results;
+        }
+        // replace main list with filter list
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            data = (ArrayList<Artist>) results.values;
+            notifyDataSetChanged();
+        }
+    }
+
+    // get arraylist after filter
+    public ArrayList<Artist> getFilterList(){
+        return data;
+    }
+    // END Filter
 }
